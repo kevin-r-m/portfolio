d0a10e1f8e7f42f82a7e88165a6a11f3ea06ff6b
kevin-r-m <kmclaughlinbtb@gmail.com>
2022-07-10 21:49:20 -0500

    styling changes, added light/night mode

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-const dropDown = document.querySelectorAll(".dropdown");
-const dropDownList = document.querySelectorAll(".nested-nav-list");
+window.onscroll = handleNav;
+const body = document.querySelector("body")
 
-dropDown.forEach(item => {
-    item.addEventListener('click', handleDropDown);
+if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    body.classList.add('night-mode')
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+    body.classList.toggle('night-mode')
 });
+
+const dropDown = document.querySelector(".dropdown");
+const dropDownList = document.querySelector(".nested-nav-list");
+
+dropDown.addEventListener('click', handleDropDown);
 
 function handleDropDown(e) {
     e.preventDefault();
-    const selectedItem = document.getElementById(e.target.dataset.destination);
-
-    selectedItem.classList.toggle('active');
-    selectedItem.classList.toggle('active');
+    dropDown.classList.toggle('active');
+    dropDownList.classList.toggle('active');
 }
 
-document.addEventListener('click', function(e){
-    if(!e.target.closest(".nav-list")){
-        handleDropDown(e);
-    }
-})
+// document.addEventListener('click', function(e){
+//     if(!e.target.closest(".nav-list")){
+//         handleDropDown(e);
+//     }
+// })
 
-window.onscroll = handleNav;
 
 function handleNav(){
     if (window.scrollY > 10){
@@ -27,4 +33,14 @@ function handleNav(){
         return
     }
     document.querySelector(".nav-list").classList.remove('active');
+}
+
+const modeToggle = document.querySelector(".mode-toggle");
+
+modeToggle.addEventListener('click', handleModeToggle);
+
+function handleModeToggle(e){
+    // e.preventDefault();
+    e.stopPropagation();
+    body.classList.toggle('night-mode')
 }
