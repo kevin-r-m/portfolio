40660152ed1a5b4a23204b064d6e2854b73b2ebf
kevin-r-m <kmclaughlinbtb@gmail.com>
2022-07-10 22:17:08 -0500

    added intersectionObeserver to handle active nav

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-window.onscroll = handleNav;
+window.addEventListener("scroll", handleNav);
 const body = document.querySelector("body")
 
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
@@ -28,11 +28,45 @@ function handleDropDown(e) {
 
 
 function handleNav(){
+
+    handleActiveNav();
+
     if (window.scrollY > 10){
         document.querySelector(".nav-list").classList.add('active');
         return
     }
     document.querySelector(".nav-list").classList.remove('active');
+}
+
+function handleActiveNav(){
+    const observedSections = document.querySelectorAll("section");
+
+    let options = {
+        root: null,
+        rootMargin: '0px',
+        threshold: .5
+      }
+
+      let callback = (entries, observer) => {
+          entries.forEach(entry => {
+              if(entry.isIntersecting){
+                document.getElementById(entry.target.dataset.navItem).classList.add('active');
+                console.log(entry.target)
+              }else {
+                document.getElementById(entry.target.dataset.navItem).classList.remove('active')
+              }
+          })
+      }
+      
+    let observer = new IntersectionObserver(callback, options);
+    
+
+    
+    observedSections.forEach(item => {
+        let target = item;
+        observer.observe(target);
+    })
+    
 }
 
 const modeToggle = document.querySelector(".mode-toggle");
